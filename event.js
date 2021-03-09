3a050ed620b1bbceaffd34e5605ba7d090fe4fc1
unknown <rozasguydavid@gmail.com>
2021-03-09 20:37:22 +0100

    my test version

--- event.js
+++ event.js
@@ -1,5 +1,33 @@
 var eventList = [];
 
+// get range of half hour
+var getHourRange = function(startDate, endDate){
+    let hours = [];
+    const date = new Date();
+    const formatter = new Intl.DateTimeFormat('en-US', {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: false
+    });
+
+    // push hour format HH and MM -> [[10, 00], [10, 30], ...]
+    for (var hour = startDate.getHours(); hour < endDate.getHours(); hour++) {
+        hours.push([hour, 0]);
+        hours.push([hour, 30]);
+    }
+
+    // get list of avaible hours HH:MM -> ["10:00", "10:30", ...]
+    const hours_range = hours.map(time => {
+        const [hour, minute] = time;
+        date.setHours(hour);
+        date.setMinutes(minute);
+
+        return formatter.format(date);
+    });
+
+    return hours_range;
+}
+
 var Event = function(opening, recurring, startDate, endDate){
   this.opening = opening;
   this.recurring = recurring;
@@ -10,5 +38,46 @@ var Event = function(opening, recurring, startDate, endDate){
 };
 
 Event.prototype.availabilities = function(fromDate, toDate){
-  return //Something awesome;
+    var result = "";
+
+    // get opening event list filterd on date range and opening true
+    const openingList = eventList.filter((event) => {
+        return event.opening == true &&
+        event.startDate.getTime() >= fromDate.getTime() &&
+        event.endDate.getTime() <= toDate.getTime();
+    });
+
+    // get opening event list without time part
+    const openingListNoTime  = openingList.map(event => ({
+        openings: event.opening,
+        recurring: event.recurring,
+        startDate: new Date(fromDate.setHours(0, 0, 0, 0)),
+        endDate: new Date(toDate.setHours(0, 0, 0, 0))
+    }));
+
+    // create a new list filterd on date range whitout time part compare
+    const openingEvent = openingListNoTime.filter((event) => event.startDate.getTime() <= fromDate.getTime());
+
+    // prepare result case
+    if (openingEvent.length !== 0) {
+        result = "I'm available from %DATE%, at %HOUR_LIST%  \nI'm not available any other time !";
+    } else {
+        result = "the company is not available on this range of date";
+    }
+
+    // prepare format date
+    var hourRange = new getHourRange(startDate, endDate);
+    const options = {month: 'long', day: 'numeric' };
+    var replacements = {
+        "%DATE%": startDate.toLocaleDateString('en-EN', options),
+        "%HOUR_LIST%": hourRange.slice(0, hourRange.length -1).concat(" and ", hourRange[hourRange.length - 1])
+    };
+
+    // replace data
+    result = result.replace(/%\w+%/g, function(all) {
+        return replacements[all] || all;
+    });
+
+    console.log(result);
+    return result;
 };
--- main.js
+++ main.js
@@ -1,19 +1,40 @@
 var startDate = new Date(2016,6,1,10,30); // July 1st, 10:30
 var endDate = new Date(2016,6,1,14,00); // July 1st, 14:00
-
 new Event(true, true, startDate, endDate); // weekly recurring opening in calendar
 
 startDate = new Date(2016,6,8,11,30); // July 8th 11:30
 endDate = new Date(2016,6,8,12,30); // July 8th 12:30
 new Event(false, false, startDate, endDate); // intervention scheduled
 
-var fromDate = new Date(2016,7,4,10,00);
-var toDate = new Date(2016,7,10,10,00);
+var startDate = new Date(2016,6,8,10,30); // July 8th, 10:30
+var endDate = new Date(2016,6,8,14,00); // July 8th, 14:00
 
-Event.prototype.availabilities(fromDate, toDate);
+new Event(true, true, startDate, endDate); // weekly recurring opening in calendar
 
+
+// test
+
+// var fromDate = new Date(2016,7,4,10,00);
+// var toDate = new Date(2016,7,10,10,00);
+// Event.prototype.availabilities(fromDate, toDate);
 /*
- * Answer should be : 
+ * Answer should be :
+ * the company is not available on this range of date
+ */
+
+// var fromDate = new Date(2020,6,4,10,00);
+// var toDate = new Date(2020,6,10,10,00);
+// Event.prototype.availabilities(fromDate, toDate);
+/*
+ * Answer should be :
+ * the company is not available on this range of date
+ */
+
+var fromDate = new Date(2016,6,4,10,00);
+var toDate = new Date(2016,6,10,10,00);
+Event.prototype.availabilities(fromDate, toDate);
+/*
+ * Answer should be :
  * I'm available from July 8th, at 10:30, 11:00, 12:30, 13:00, and 13:30
  * I'm not available any other time !
  */
